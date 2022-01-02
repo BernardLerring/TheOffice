9379dd7adc519fe22c2f58097d5d93e23f5f90e3
Tim <orcatim@hotmail.com>
2022-01-02 17:42:32 +0000

    add basic JS structure to generate quiz

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+function buildQuiz(){}
+
+function showResults(){}
+
+
+
+buildQuiz();
+
+submitButton.addEventListener('click', showResults);
+
+
